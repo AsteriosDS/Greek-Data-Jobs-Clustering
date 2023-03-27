0ee7374074b83841438dd57ba4f3a46f3156edaf
AsteriosDS <117105637+AsteriosDS@users.noreply.github.com>
2023-03-28 02:32:46 +0300

    Update job_board2.py

--- job_board2.py
+++ job_board2.py
@@ -237,5 +237,6 @@ with tab3:
     # Convert the overlap matrix to a DataFrame
     mt = pd.DataFrame(overlap_matrix, columns=titles, index=titles)
     
-    fig = sns.heatmap(mt, annot=True, cmap='coolwarm', fmt='.2f')
+    fig = plt.figure()
+    sns.heatmap(mt, annot=True, cmap='coolwarm', fmt='.2f')
     st.pyplot(fig)

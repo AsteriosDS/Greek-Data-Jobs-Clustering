c2d57618a9d5d376b79d7e6da58982899b8c4e3c
AsteriosDS <117105637+AsteriosDS@users.noreply.github.com>
2023-03-28 12:31:00 +0300

    Update job_board2.py

--- job_board2.py
+++ job_board2.py
@@ -242,6 +242,6 @@ with tab3:
     om = pd.DataFrame(overlap_matrix, columns=job_titles, index=job_titles)
     
     with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(5, 8))
+        f, ax = plt.subplots(figsize=(8, 5))
         ax = sns.heatmap(om, vmax=1, square=True, annot=True, cmap='coolwarm')
     st.pyplot(f)
